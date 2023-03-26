67975e80e4de7c6c1de23caa2939e5a701825a74
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-03-26 16:30:49 +0300

    Add new argument -f/--format

--- gendiff/scripts/engine.py
+++ gendiff/scripts/engine.py
@@ -8,6 +8,7 @@ def parse():
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
 
     return args
