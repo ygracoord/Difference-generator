cbaa573162eef45ab670555d11f3762a25148444
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-04-03 18:45:36 +0300

    Add test for yaml

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,10 +1,19 @@
 from gendiff.gendiff import generate_diff
 
+FLAT_JSON1, FLAT_JSON2 = ['tests/fixtures/file1.json', 'tests/fixtures/file2.json']
+FLAT_JSON_RESULT = 'tests/fixtures/result_json.txt'
 
-def test_json():
-    first_json = 'tests/fixtures/file1.json'
-    second_json = 'tests/fixtures/file2.json'
-    result = 'tests/fixtures/result_json.txt'
+FLAT_YML1, FLAT_YML2 = ['tests/fixtures/file1.yml', 'tests/fixtures/file2.yml']
+FLAT_YAML_RESULT = 'tests/fixtures/result_yaml.txt'
 
-    with open(result) as result_json:
-        assert generate_diff(first_json, second_json) == result_json.read()
+
+def test_flat_json():
+    with open(FLAT_JSON_RESULT) as file:
+        expected = file.read()
+        assert expected == generate_diff(FLAT_JSON1, FLAT_JSON2)
+
+
+def test_flat_yaml():
+    with open(FLAT_YAML_RESULT) as file:
+        expected = file.read()
+        assert expected == generate_diff(FLAT_YML1, FLAT_YML2)
