b026ab385859fe97df3acb9f4d190cb4c1497e15
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-04-03 18:41:45 +0300

    Divide the module into parts

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,32 +1,21 @@
-import json
-import os
+from gendiff.file_operations.file_parser import get_file
 
 
-def check_extension(file):
-    _, extension = os.path.splitext(file)
-    if extension != '.json':
-        raise ValueError('Incorrect file format')
-    with open(file) as f:
-        file = json.load(f)
-
-    return file
-
-
-def generate_diff(path_1, path_2):
-    file_1, file_2 = check_extension(path_1), check_extension(path_2)
+def generate_diff(path_1: str, path_2: str) -> str:
+    file_1, file_2 = get_file(path_1), get_file(path_2)
     keys = set(file_1.keys()) | set(file_2.keys())
     diff = {key: (file_1.get(key), file_2.get(key)) for key in sorted(keys)}
     result = []
 
     for key, (value_1, value_2) in diff.items():
         if value_1 is None:
-            result.append(f'+ {key}: {value_2}')
+            result.append(f'+ {key}: {str(value_2).lower()}')
         elif value_2 is None:
-            result.append(f'- {key}: {value_1}')
+            result.append(f'- {key}: {str(value_1).lower()}')
         elif value_1 != value_2:
-            result.append(f'- {key}: {value_1}')
-            result.append(f'+ {key}: {value_2}')
+            result.append(f'- {key}: {str(value_1).lower()}')
+            result.append(f'+ {key}: {str(value_2).lower()}')
         else:
-            result.append(f'  {key}: {value_1}')
+            result.append(f'  {key}: {str(value_1).lower()}')
 
     return '{\n  ' + '\n  '.join(result) + '\n}'
