61d399600a215dd1bbd96b3bb7d18abf7cb10882
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-04-03 18:37:32 +0300

    Add file operations dir

---
+++ gendiff/file_operations/file_parser.py
@@ -0,0 +1,21 @@
+import os
+import json
+import yaml
+
+
+EXTENSIONS = ('.json', '.yaml', '.yml')
+
+
+def get_file(file_path: str) -> dict:
+    _, extension = os.path.splitext(file_path)
+    if extension not in EXTENSIONS:
+        raise ValueError(f"Incorrect file format: {extension}")
+    try:
+        with open(file_path) as f:
+            if extension == '.json':
+                file = json.load(f)
+            elif extension == '.yml' or extension == '.yaml':
+                file = yaml.safe_load(f)
+            return file
+    except (FileNotFoundError, json.JSONDecodeError, yaml.YAMLError, UnicodeDecodeError) as error:
+        raise error
