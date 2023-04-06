53216915808cfd348083a83365a67925f82b123d
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-04-06 10:21:27 +0300

    Share logic, add new module

--- gendiff/scripts/engine.py
+++ gendiff/scripts/engine.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env python3
-import argparse
+from gendiff.cli import cli_parse
 from gendiff.gendiff import generate_diff
 
 
-def parse():
-    parser = argparse.ArgumentParser(
-        description='Compares two configuration files and shows a difference.'
-    )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
-
-    return args.first_file, args.second_file
-
-
 def main():
-    first, second = parse()
-    print(generate_diff(first, second))
+    args = cli_parse()
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == '__main__':
