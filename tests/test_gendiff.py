dfd67f84fbb1bee562017c182e6222016040f430
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-04-14 09:31:48 +0300

    Refactor tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,20 +2,19 @@ from gendiff.file_operations.gendiff import generate_diff
 
 FLAT_JSON1 = 'tests/fixtures/input_files/file1.json'
 FLAT_JSON2 = 'tests/fixtures/input_files/file2.json'
-FLAT_JSON_RESULT = 'tests/fixtures/output_files/result_json.txt'
-
 FLAT_YML1 = 'tests/fixtures/input_files/file1.yml'
 FLAT_YML2 = 'tests/fixtures/input_files/file2.yml'
-FLAT_YAML_RESULT = 'tests/fixtures/output_files/result_yaml.txt'
+
+RESULT_STYLISH = 'tests/fixtures/output_files/stylish.txt'
 
 
 def test_flat_json():
-    with open(FLAT_JSON_RESULT) as file:
+    with open(RESULT_STYLISH) as file:
         expected = file.read()
         assert expected == generate_diff(FLAT_JSON1, FLAT_JSON2)
 
 
 def test_flat_yaml():
-    with open(FLAT_YAML_RESULT) as file:
+    with open(RESULT_STYLISH) as file:
         expected = file.read()
         assert expected == generate_diff(FLAT_YML1, FLAT_YML2)
