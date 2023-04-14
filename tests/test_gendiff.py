145f47a8b00cbd578f41576741b278abcb96c845
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-04-14 09:23:32 +0300

    Refactor test file

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,10 +1,12 @@
-from gendiff.gendiff import generate_diff
+from gendiff.file_operations.gendiff import generate_diff
 
-FLAT_JSON1, FLAT_JSON2 = ['tests/fixtures/file1.json', 'tests/fixtures/file2.json']
-FLAT_JSON_RESULT = 'tests/fixtures/result_json.txt'
+FLAT_JSON1 = 'tests/fixtures/input_files/file1.json'
+FLAT_JSON2 = 'tests/fixtures/input_files/file2.json'
+FLAT_JSON_RESULT = 'tests/fixtures/output_files/result_json.txt'
 
-FLAT_YML1, FLAT_YML2 = ['tests/fixtures/file1.yml', 'tests/fixtures/file2.yml']
-FLAT_YAML_RESULT = 'tests/fixtures/result_yaml.txt'
+FLAT_YML1 = 'tests/fixtures/input_files/file1.yml'
+FLAT_YML2 = 'tests/fixtures/input_files/file2.yml'
+FLAT_YAML_RESULT = 'tests/fixtures/output_files/result_yaml.txt'
 
 
 def test_flat_json():
