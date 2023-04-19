4fe9a51b062c24f2bc1b727db5c204025edd72c7
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-04-19 13:06:37 +0300

    Fix vars and conditions

--- gendiff/file_operations/file_parser.py
+++ gendiff/file_operations/file_parser.py
@@ -2,7 +2,9 @@ import os
 import json
 import yaml
 
-EXTENSIONS = ('.json', '.yaml', '.yml')
+JSON = '.json'
+YAML_OR_YML = ('.yaml', '.yml')
+EXTENSIONS = (JSON, YAML_OR_YML)
 
 
 def get_file(file_path: str) -> dict:
@@ -11,9 +13,9 @@ def get_file(file_path: str) -> dict:
         raise ValueError(f"Incorrect file format: {extension}")
     try:
         with open(file_path) as f:
-            if extension == '.json':
+            if extension == JSON:
                 file = json.load(f)
-            elif extension == '.yml' or extension == '.yaml':
+            elif extension in YAML_OR_YML:
                 file = yaml.safe_load(f)
             return file
     except (FileNotFoundError, json.JSONDecodeError, yaml.YAMLError) as error:
