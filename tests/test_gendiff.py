907236df4ef39aa37c4af4023c710c05f3f57252
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-05-02 17:40:05 +0300

    Add tests for attached data

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,20 +1,29 @@
+import pytest
+
 from gendiff.file_operations.gendiff import generate_diff
+from gendiff.formatters.rendering import STYLISH
 
 FLAT_JSON1 = 'tests/fixtures/input_files/file1.json'
 FLAT_JSON2 = 'tests/fixtures/input_files/file2.json'
 FLAT_YML1 = 'tests/fixtures/input_files/file1.yml'
 FLAT_YML2 = 'tests/fixtures/input_files/file2.yml'
+ATTACHED_JSON1 = 'tests/fixtures/input_files/file3.json'
+ATTACHED_JSON2 = 'tests/fixtures/input_files/file4.json'
+ATTACHED_YML1 = 'tests/fixtures/input_files/file3.yml'
+ATTACHED_YML2 = 'tests/fixtures/input_files/file4.yml'
 
 RESULT_STYLISH = 'tests/fixtures/output_files/stylish.txt'
+RESULT_STYLISH_ATTACHED = 'tests/fixtures/output_files/stylish_attached.txt'
 
 
-def test_flat_json():
-    with open(RESULT_STYLISH) as file:
+@pytest.mark.parametrize('file_1, file_2, format, result_file', [
+    (FLAT_JSON1, FLAT_JSON2, STYLISH, RESULT_STYLISH),
+    (FLAT_YML1, FLAT_YML2, STYLISH, RESULT_STYLISH),
+    (FLAT_YML1, FLAT_JSON2, STYLISH, RESULT_STYLISH),
+    (ATTACHED_JSON1, ATTACHED_JSON2, STYLISH, RESULT_STYLISH_ATTACHED),
+    (ATTACHED_YML1, ATTACHED_YML2, STYLISH, RESULT_STYLISH_ATTACHED),
+])
+def test_generate_diff(file_1, file_2, format, result_file):
+    with open(result_file) as file:
         expected = file.read()
-        assert expected == generate_diff(FLAT_JSON1, FLAT_JSON2)
-
-
-def test_flat_yaml():
-    with open(RESULT_STYLISH) as file:
-        expected = file.read()
-        assert expected == generate_diff(FLAT_YML1, FLAT_YML2)
+        assert expected == generate_diff(file_1, file_2, format)
