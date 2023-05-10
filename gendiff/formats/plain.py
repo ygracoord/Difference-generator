7cf6badb7b631da65ff5e53b3e7e2e6123c8cef3
danilduborezov <95632838+ygracoord@users.noreply.github.com>
2023-05-10 10:46:11 +0300

    Add new format

---
+++ gendiff/formats/plain.py
@@ -0,0 +1,59 @@
+from typing import Any
+
+from gendiff.file_operations.diff import ChangeType
+
+PATH = '{}.{}'
+COMPLEX_VALUE = "[complex value]"
+TEMPLATES = {
+    'added': "Property '{}' was added with value: {}",
+    'deleted': "Property '{}' was removed",
+    'changed': "Property '{}' was updated. From {} to {}",
+}
+
+
+def render_nodes(diff: list, root='') -> str:
+    result = []
+    diff.sort(key=lambda node: node['key'])
+
+    for node in diff:
+        path = PATH.format(root, node['key']) if root else node['key']
+
+        if node['type'] == ChangeType.ADDED:
+            result.append(TEMPLATES['added'].format(
+                path, transform(node['value']['new'])
+            ))
+
+        elif node['type'] == ChangeType.DELETED:
+            result.append(TEMPLATES['deleted'].format(path))
+
+        elif node['type'] == ChangeType.CHANGED:
+            result.append(TEMPLATES['changed'].format(
+                path,
+                transform(node['value']['old']),
+                transform(node['value']['new'])
+            ))
+
+        elif node['type'] == ChangeType.ATTACHED:
+            result.append(render_nodes(node['children'], path))
+
+    return '\n'.join(result)
+
+
+def render_plain(diff_tree: list) -> str:
+    finished_data = render_nodes(diff_tree)
+    return finished_data
+
+
+def transform(value: Any) -> str:
+    if isinstance(value, bool):
+        correct_value = str(value).lower()
+    elif value is None:
+        correct_value = 'null'
+    elif isinstance(value, int):
+        correct_value = str(value)
+    elif isinstance(value, dict):
+        correct_value = COMPLEX_VALUE
+    else:
+        correct_value = f"'{value}'"
+
+    return correct_value
--- gendiff/formats/rendering.py
+++ gendiff/formats/rendering.py
@@ -1,9 +1,14 @@
 from gendiff.formats.stylish import render_stylish
+from gendiff.formats.plain import render_plain
 
 STYLISH = 'stylish'
+PLAIN = 'plain'
+JSON = 'json'
 DEFAULT = STYLISH
 
 
-def render_tree(diff: list[dict], format: str) -> str:
+def render_tree(diff_tree: list[dict], format: str) -> str:
     if format == STYLISH:
-        return render_stylish(diff)
+        return render_stylish(diff_tree)
+    elif format == PLAIN:
+        return render_plain(diff_tree)
